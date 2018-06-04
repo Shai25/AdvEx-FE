00c7d2e683c33de01c8d818ba90e547101fe253d
Linghao Zhang <linghaozh@gmail.com>
2018-06-03 23:02:35 -0400

    intial commit

---
+++ src/layout/dashboard/ContentFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid d-flex flex-wrap justify-content-between">
+      <nav>
+        <ul>
+          <li>
+            <router-link :to="{path:'/admin'}">AdvEx Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright d-flex flex-wrap">
+        &copy; 2018 AdvEx Team. All rights reserved.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,51 @@
+import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
+// GeneralViews
+import NotFound from "@/pages/NotFoundPage.vue";
+
+// Admin pages
+import Dashboard from "@/pages/Dashboard.vue";
+import UserProfile from "@/pages/UserProfile.vue";
+import SubmissionHistory from "@/pages/SubmissionHistory.vue";
+import SubmissionDetail from "@/pages/SubmissionDetail.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: Dashboard
+      },
+      {
+        path: "profile",
+        name: "User Profile",
+        component: UserProfile
+      },
+      {
+        path: "history",
+        name: "Submission History",
+        component: SubmissionHistory
+      },
+      {
+        path: "submission",
+        name: "Submission Detail",
+        component: SubmissionDetail
+      }
+    ]
+  },
+  { path: "*", component: NotFound }
+];
+
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * The specified component must be inside the Views folder
+ * @param  {string} name  the filename (basename) of the view to load.
+function view(name) {
+   var res= require('../components/Dashboard/Views/' + name + '.vue');
+   return res;
+};**/
+
+export default routes;
