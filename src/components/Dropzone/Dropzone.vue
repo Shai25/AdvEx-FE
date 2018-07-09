26923e5b7d290c1ac345cb8260a309b4f215b837
Linghao Zhang <linghaozh@gmail.com>
2018-07-09 06:15:27 -0400

    fix timeout in upload

--- src/components/Dropzone/Dropzone.vue
+++ src/components/Dropzone/Dropzone.vue
@@ -111,6 +111,8 @@ export default {
           _send.call(xhr, file);
         }
       },
+      // Timeout of 5 minutes
+      timeout: 300000;
       // Upload one file at a time since we're using the S3 pre-signed URL scenario
       parallelUploads: 1,
       uploadMultiple: false,
